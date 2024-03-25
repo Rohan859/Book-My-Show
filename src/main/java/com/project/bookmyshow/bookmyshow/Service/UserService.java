2131230b4de9d4a8e75d6f3b82185be7e880ca29
Rohan859 <rohankundu859@gmail.com>
2024-03-25 12:38:27 +0530

    added email notification

--- src/main/java/com/project/bookmyshow/bookmyshow/Service/UserService.java
+++ src/main/java/com/project/bookmyshow/bookmyshow/Service/UserService.java
@@ -2,11 +2,8 @@ package com.project.bookmyshow.bookmyshow.Service;
 
 import com.project.bookmyshow.bookmyshow.Entities.User;
 import com.project.bookmyshow.bookmyshow.Repository.UserRepository;
-import com.twilio.http.TwilioRestClient;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.rest.api.v2010.account.MessageCreator;
-import com.twilio.type.PhoneNumber;
-import com.twilio.Twilio;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -21,9 +18,9 @@ public class UserService
     @Autowired
     JavaMailSender javaMailSender;
 
-
-    final static String ACCOUNT_SID = "AC9e067598cbd5ba28c088407a245a42dc";
-    final static String  AUTH_TOKEN = "2f40d6a9ecd4bd54cfe96040dbc883f6";
+//
+//    final static String ACCOUNT_SID = "AC9e067598cbd5ba28c088407a245a42dc";
+//    final static String  AUTH_TOKEN = "2f40d6a9ecd4bd54cfe96040dbc883f6";
 
 
 
@@ -52,13 +49,15 @@ public class UserService
 
 
 
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        Message message1 = Message.creator(
-                        new com.twilio.type.PhoneNumber("+91"+user.getMobileNo()),
-                        new com.twilio.type.PhoneNumber("+14434007303"),
-                        body)
-                .create();
+//        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+//
+//
+//        Message message1 = Message.creator(
+//                        new com.twilio.type.PhoneNumber("+91"+user.getMobileNo()),
+//                        new com.twilio.type.PhoneNumber("+14434007303"),
+//                        body)
+//                .create();
 
         return "successfully added the user with id "+user.getUserId();
     }
